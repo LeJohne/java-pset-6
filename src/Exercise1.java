c6fb19e691cd4e24ee25bd5f8786cc670195b55b
LeJohne <johngoy11024@gmail.com>
2020-10-18 19:12:56 -0400

    exercise 1 - done

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,2 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("###,###,###.##");
+
+        int lower = 0;
+        int upper = -1;
+        int sum = 0;
+
+        while(upper < lower) {
+            System.out.print("Lower bound: ");
+            lower = in.nextInt();
+
+            System.out.print("Upper bound: ");
+            upper = in.nextInt();
+        }
+
+        for (int x = lower; x <= upper ; x++ ){
+            if (x % 2 == 0){
+                sum += x;
+            }
+        }
+
+        System.out.println("\n" + deci.format(sum) + ".");
+
+        in.close();
+    }
 }
