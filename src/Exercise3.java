4e0156e7c583bb2b0f8b9354a34daf43f70774c6
LeJohne <johngoy11024@gmail.com>
2020-10-18 19:57:37 -0400

    exercise 3 - done

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int integer;
+        int sum = 0;
+
+        do{
+            System.out.print("Enter an integer: ");
+            integer = in.nextInt();
+        } while (integer <= 0);
+
+        String userString = String.valueOf(integer);
+        int length = userString.length();
+
+        for(int x = 0; x < length; x++){
+
+            String intSub = userString.substring(x, x+1);
+            int intAtString = Integer.parseInt(intSub);
+
+            if(intAtString % 2 != 0){
+                sum = sum + intAtString;
+            }
+        }
+        System.out.println("\n" + sum + ".");
+        in.close();
+    }
 }
