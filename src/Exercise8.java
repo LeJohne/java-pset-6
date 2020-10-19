c3d180724b69a9a3d5a32a697586e662fc48b43c
LeJohne <johngoy11024@gmail.com>
2020-10-18 22:05:07 -0400

    exercise 8 & 7 swapped

---
+++ src/Exercise8.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int height;
+        int row;
+        int variablex = 1;
+        int y;
+        int blank = 1;
+        String blank2 = "";
+
+        do{
+            System.out.print("Height: ");
+            height = in.nextInt();
+        } while (height < 1 || height > 24 || height % 1 != 0);
+
+        row = height + 1;
+        y = 2;
+
+        for(int x = 0; x < height; x++){
+            while(blank <= row - y) {
+                blank2 = blank2 + " ";
+                blank++;
+            }
+            while (variablex <= y){
+                blank2 = blank2 + "#";
+                variablex++;
+            }
+            System.out.print("\n" + blank2);
+            y += 1;
+            blank2 = "";
+            blank = 1;
+            variablex = 1;
+        }
+        in.close();
+    }
+}
