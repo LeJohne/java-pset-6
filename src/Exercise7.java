75a689018df51a2aacaf830a8eb83eba0128396b
LeJohne <johngoy11024@gmail.com>
2020-10-18 22:00:04 -0400

    exercise 8 - done

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -4,21 +4,36 @@ public class Exercise7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int integer;
+        int height;
+        int row;
+        int variablex = 1;
+        int y;
+        int blank = 1;
+        String blank2 = "";
+
         do{
-            System.out.print("Enter an integer: ");
-            integer = in.nextInt();
-        } while (integer <= 0);
+            System.out.print("Height: ");
+            height = in.nextInt();
+        } while (height < 1 || height > 24 || height % 1 != 0);
 
-        System.out.print("\n1, " + integer);
+        row = height + 1;
+        y = 2;
 
-        for(int x = 2; x <= (integer / x); x++) {
-            if (integer % x == 0) {
-                int div = integer / x;
-                System.out.print(", " + x + ", " + div);
+        for(int x = 0; x < height; x++){
+            while(blank <= row - y) {
+                blank2 = blank2 + " ";
+                blank++;
             }
+            while (variablex <= y){
+                blank2 = blank2 + "#";
+                variablex++;
+            }
+            System.out.print("\n" + blank2);
+            y += 1;
+            blank2 = "";
+            blank = 1;
+            variablex = 1;
         }
-        System.out.print(".");
         in.close();
     }
 }
